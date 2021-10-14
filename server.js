a1c5d7683c19f2e0b8651270256df0fb66c6f070
Ali Jouni <alijouni@outlook.de>
2021-10-14 23:50:02 +0300

    Connecting Database to Server

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-import express from 'express';
+import express, { response } from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import knex from 'knex';
 
-
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'postgres',
+      password : 'rs90250',
+      database : 'smart_brain'
+    }
+});
+  
 const app = express();
 
 app.use(express.json());
@@ -49,43 +59,44 @@ app.post('/signin', (req, res) => {
 app.post('/register', (req, res) => {
     
     const { email, name, password } = req.body;
-    database.users.push({
-        id: '125',
-        name: name,
+    db('users')
+        .returning('*')
+        .insert({
         email: email,
-        entries: 0,
+        name: name,
         joined: new Date()
-    })
-    res.json(database.users[database.users.length-1]);
+
+        })
+        .then(user => {
+            res.json(user[0]);   
+        })
+        .catch(err => res.status(400).json('Unable to register'))
+   
 })
 
 app.get('/profile/:id', (req, res) => {
     const { id } = req.params;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            return res.json(user);
-        } 
-    })
-    if (!found) {
-        res.status(404).json('No such user');
-    }
+    db.select('*').from('users').where({ id })
+        .then(user => {
+            if (user.length) {
+                res.json(user[0])
+            } else {
+                res.status(400).json('not found')
+            }   
+        })
+        .catch(err=> res.status(400).json('error getting user'))
 })
 
 app.put('/image', (req, res) => {
     const { id } = req.body;
-    let found = false;
-    database.users.forEach(user => {
-        if (user.id === id) {
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        } 
-    })
-    if (!found) {
-        res.status(404).json('No such user');
-    }
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            res.json(entries[0]);
+        })
+    .catch(err=>res.status(400).json('Unable to get entries'))
+    
 })
 
 app.listen(3001, () => {
