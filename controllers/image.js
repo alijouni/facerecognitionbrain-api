a911bf264e5946fc784935fcf3ffa339a1251dd6
Ali Jouni <alijouni@outlook.de>
2021-11-19 18:52:07 +0200

    test upload entry count

--- controllers/image.js
+++ controllers/image.js
@@ -1,36 +1,9 @@
 import Clarifai from 'clarifai';
-// import {useEffect} from 'react';
 
 const app = new Clarifai.App({
     apiKey: process.env.API_CLARIFAI_KEY,
 });
 
-const useEffect = (callback, dependencies) => {
-    // Calling it first time since there are no dependency
-    if (dependencies === undefined) {
-      return callback();
-    }
-  
-    // Creating proxy for setters
-    return new Proxy(dependencies, {
-      set: function (target, key, value) {
-        Reflect.set(target, key, value);
-        callback();
-      },
-    });
-};
-  
-const handleApiCall64 = (req, res) => {
-    useEffect(() => {
-        app.models
-            .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-            .then(data => {
-                res.json(data);
-                console.log(data);
-            })
-            .catch(err => res.status(400).json('Unable to work with API'))
-    }, [req.body.input])
-}
 
 const handleApiCall = (req, res) => {
     app.models
@@ -39,19 +12,6 @@ const handleApiCall = (req, res) => {
             res.json(data);
         })
     .catch(err=>res.status(400).json('Unable to work with API'))
-    // useEffect(() => {
-        
-        
-    //     app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    //     .then(data => {
-    //         res.json(data);
-    //     })
-    // .catch(err=>res.status(400).json('Unable to work with API'))
-    
-    
-    
-    // }, [req.body.input]);
-
 }
 
 const handleImage = (req, res, db) => {
--- server.js
+++ server.js
@@ -39,7 +39,6 @@ app.get('/profile/:id', (req,res)=>{handleProfileGet(req,res,db)});
 
 app.put('/image', (req, res) => { handleImage(req, res, db) });
 app.post('/imageurl', (req,res)=>{handleApiCall(req,res)});
-app.post('/image64url', (req,res)=>{handleApiCall64(req,res)});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on port ${process.env.PORT}`);
